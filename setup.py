a9217d58dab3c94f390b473a10e80bf4f9d08c99
MathijsTak <mathijs.tak@outlook.com>
2021-10-19 23:47:46 +0200

    First release

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10/11',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
 
-setup(name='zerooneai',
-    version='0.1',
+setup(
+    name='zerooneai',
+    version='0.0.1',
     description='Whith this package you can make an simple AI that can predict a zero or a one.',
-    long_description=readme(),
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELONG.txt').read(),
     url='https://github.com/MathijsTak/Pacemaker-ai',
     author='MathijsTak',
     author_email='mathijs.tak@outlook.com',
-    license='Open source',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='zeroone zero one ai zerooneai zeroai oneai',
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
         'matplotlib',
         'sklearn',
         'pickle'
-    ],
-    keywords='zeroone zero one ai zerooneai zeroai oneai',
-    packages=['zerooneai'],
-    package_dir={'zerooneai': 'src/zerooneai'}
+    ]
 )
